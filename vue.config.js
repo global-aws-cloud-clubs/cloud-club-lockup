1fe5732c7a79456639b751c22965548621389c26
jlooper <jen.looper@gmail.com>
2024-01-28 10:35:50 -0700

    path

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cloud-club-lockup/'
-    : '/cloud-club-lockup/'
+    : '/'
 })
