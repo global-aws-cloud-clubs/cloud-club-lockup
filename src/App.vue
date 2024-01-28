266ef4c759b3ff395964c090918ffc810d7cd182
jlooper <jen.looper@gmail.com>
2024-01-27 22:16:26 -0700

    new app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LockupGenerator/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LockupGenerator from './components/LockupGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LockupGenerator
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Amazon Ember, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  margin-left: 10px;
+}
+.club-name {
+  font-size: 3em;
 }
 </style>
